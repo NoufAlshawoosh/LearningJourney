3b1a8b5edf58c48bfa00eefb911a61e2e447252f
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2025-10-17 19:50:48 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Nouf Alshawoosh on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
